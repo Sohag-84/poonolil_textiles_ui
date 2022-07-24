de6218fbe0f3f2ff12da8d4ab74715cb1d8d2a9b
IH Sohag <injamulhaqsohag19977@gmail.com>
2022-07-24 10:00:04 +0600

    setup screen util and getx

---
+++ android/app/src/main/kotlin/com/example/poonolil_textiles_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.poonolil_textiles_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.red));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      builder: (BuildContext context, Widget? child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Center(child: Text("data")),
+        );
+      },
+    );
+  }
+}
